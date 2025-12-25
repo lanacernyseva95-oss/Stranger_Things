58ff3bfe4087fdf7dfeabfd04148ab3163185fe0
lanacernyseva95-oss <lanacernyseva95@gmail.com>
2025-12-25 16:59:29 +0300

    added animation text

---
+++ script.js
@@ -0,0 +1,22 @@
+const head = document.querySelector("h1")
+let text = head.textContent
+head.innerHTML = ""
+
+const chars = text.split("")
+console.log(chars)
+animate(chars, {
+  // Property keyframes
+  y: [
+    { to: '-2.75rem', ease: 'outExpo', duration: 600 },
+    { to: 0, ease: 'outBounce', duration: 800, delay: 100 }
+  ],
+  // Property specific parameters
+  rotate: {
+    from: '-1turn',
+    delay: 0
+  },
+  delay: stagger(50),
+  ease: 'inOutCirc',
+  loopDelay: 1000,
+  loop: true
+});
